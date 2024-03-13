6cc6e81cc7099b61f3e8d362bfe0aed33970dfd2
RafaelVS <160777850+RafaelANTVS@users.noreply.github.com>
2024-03-13 16:10:04 -0300

    Create Menu
    O menu de fato

---
+++ Menu.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int opcao;
+  do{
+    printf("1 - Menu Principal\n");
+    printf("2 - Cadastrar\n");
+    scanf(" %d", &opcao);
+
+  }while(opcao != 0);
+  return 0;
+}
