e15862c45463b05bf5cfacd81220bc63a71e8e1e
RafaelVS <160777850+RafaelANTVS@users.noreply.github.com>
2024-03-22 18:14:22 -0300

    Update tarefas.c

--- tarefas.c
+++ tarefas.c
@@ -1,12 +1,12 @@
 #include "tarefas.h"
 #include <stdio.h>
+#include <string.h>
 
-Erro criar(Tarefa t[], int *pos){
-  if(*pos >= TOTAL)
+Erro criar(Tarefa t[], int *pos) {
+  if (*pos >= TOTAL)
     return MAX_TAREFAS;
 
-
-  printf("Digite a prioridade: ");
+  printf("Entre com uma prioridade: ");
   scanf("%d", &t[*pos].prioridade);
   clearBuffer();
 
@@ -17,39 +17,92 @@ Erro criar(Tarefa t[], int *pos){
   printf("Entre com a categoria: ");
   fgets(t[*pos].categoria, 100, stdin);
 
-  printTarefa(t[*pos]);
+  // printTarefa(t[*pos]);
   *pos = *pos + 1;
 
   return OK;
 }
-Erro deletar(Tarefa t[], int *pos){
-  printf("funcao deletar tarefa");
-  return OK;
-}
-Erro listar(Tarefa t[], int pos){
-  if(pos == 0)
+
+Erro deletar(Tarefa t[], int *pos) {
+  if (*pos == 0)
     return SEM_TAREFAS;
 
-  for(int i = 0; i < pos; i++)
-    printTarefa(t[i]);
+  printf("Entre com a posicao da tarefa: ");
+  int pos_d;
+  scanf("%d", &pos_d);
+  pos_d--;
+  if (pos_d >= *pos)
+    return NAO_ENCONTRADO;
+
+  for (int i = pos_d; i < *pos; i++) {
+    t[i].prioridade = t[i + 1].prioridade;
+    strcpy(t[i].categoria, t[i + 1].categoria);
+    strcpy(t[i].descricao, t[i + 1].descricao);
+  }
+
+  *pos = *pos - 1;
 
   return OK;
 }
-Erro salvar(Tarefa t[], int pos, int tamanho){
-  printf("funcao salvar tarefa");
-  return OK;
-}
-Erro carregar(Tarefa t[], int *pos, int tamanho){
-  printf("funcao carregar tarefa");
+
+Erro listar(Tarefa t[], int pos) {
+  if (pos == 0)
+    return SEM_TAREFAS;
+
+  for (int i = 0; i < pos; i++)
+    printTarefa(t[i], i + 1);
+
   return OK;
 }
 
-void printTarefa(Tarefa t){
-  printf("Prioridade: %d\t Categoria: %s\n", t.prioridade, t.categoria);
-  printf("Descricao: %s\n", t.descricao);
+Erro salvar(Tarefa t[], int pos, int tamanho) {
+  FILE *f = fopen("tarefas", "wb");
+  if (f == NULL)
+    return ABRIR;
+
+  int i = fwrite(t, tamanho, sizeof(Tarefa), f);
+  if (i <= 0)
+    return ESCREVER;
+
+  i = fwrite(&pos, 1, sizeof(int), f);
+  if (i <= 0)
+    return ESCREVER;
+
+  i = fclose(f);
+  if (i != 0)
+    return FECHAR;
+
+  return OK;
 }
 
-void clearBuffer(){
+Erro carregar(Tarefa t[], int *pos, int tamanho) {
+  FILE *f = fopen("tarefas", "rb");
+  if (f == NULL)
+    return ABRIR;
+
+  int i = fread(t, tamanho, sizeof(Tarefa), f);
+  if (i <= 0)
+    return LER;
+
+  i = fread(pos, 1, sizeof(int), f);
+  if (i <= 0)
+    return LER;
+
+  i = fclose(f);
+  if (i != 0)
+    return FECHAR;
+
+  return OK;
+}
+
+void printTarefa(Tarefa t, int pos) {
+  printf("\nPosicao: %d\t", pos);
+  printf("Prioridade: %d\tCategoria: %s\t", t.prioridade, t.categoria);
+  printf("Descricao: %s", t.descricao);
+}
+
+void clearBuffer() {
   int c;
-  while ((c = getchar()) != '\n' && c != EOF) {}
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
 }
