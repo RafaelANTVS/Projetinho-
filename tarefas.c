4252ec2dbb5da99a3c8f7dff16a1c9e6fe5151a0
RafaelVS <160777850+RafaelANTVS@users.noreply.github.com>
2024-03-22 21:08:04 +0000

    menu do codigo

---
+++ tarefas.c
@@ -0,0 +1,55 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+Erro criar(Tarefa t[], int *pos){
+  if(*pos >= TOTAL)
+    return MAX_TAREFAS;
+
+
+  printf("Digite a prioridade: ");
+  scanf("%d", &t[*pos].prioridade);
+  clearBuffer();
+
+  printf("Entre com a descricao: ");
+  fgets(t[*pos].descricao, 300, stdin);
+  clearBuffer();
+
+  printf("Entre com a categoria: ");
+  fgets(t[*pos].categoria, 100, stdin);
+
+  printTarefa(t[*pos]);
+  *pos = *pos + 1;
+
+  return OK;
+}
+Erro deletar(Tarefa t[], int *pos){
+  printf("funcao deletar tarefa");
+  return OK;
+}
+Erro listar(Tarefa t[], int pos){
+  if(pos == 0)
+    return SEM_TAREFAS;
+
+  for(int i = 0; i < pos; i++)
+    printTarefa(t[i]);
+
+  return OK;
+}
+Erro salvar(Tarefa t[], int pos, int tamanho){
+  printf("funcao salvar tarefa");
+  return OK;
+}
+Erro carregar(Tarefa t[], int *pos, int tamanho){
+  printf("funcao carregar tarefa");
+  return OK;
+}
+
+void printTarefa(Tarefa t){
+  printf("Prioridade: %d\t Categoria: %s\n", t.prioridade, t.categoria);
+  printf("Descricao: %s\n", t.descricao);
+}
+
+void clearBuffer(){
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {}
+}
